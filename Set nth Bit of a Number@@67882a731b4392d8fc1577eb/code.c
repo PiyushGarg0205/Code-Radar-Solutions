9c334ed060ff9d8220480110ff924e0c882eeddc
PiyushGarg0205 <piyushgarg0205@gmail.com>
2025-02-08 18:20:14 +0530

    Solution to Set nth Bit of a Number

--- Set nth Bit of a Number@@67882a731b4392d8fc1577eb/code.c
+++ Set nth Bit of a Number@@67882a731b4392d8fc1577eb/code.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 int main() {
-    int num,nth;
-    scanf("%d",&num,&nth);
-    int sb=1<<nth;
-    int newnum=num|sb;
-    printf("%d",newnum);
+    int num, nth;
+    scanf("%d %d", &num, &nth);
+    int sb = 1 << nth;
+    int newnum = num | sb;
+    printf("%d\n", newnum);
+    return 0;
 }
