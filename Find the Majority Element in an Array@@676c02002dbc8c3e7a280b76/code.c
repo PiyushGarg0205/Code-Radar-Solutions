48cded2c2be6eb0ea62e5053fa392ec33716c171
PiyushGarg0205 <piyushgarg0205@gmail.com>
2025-02-12 09:07:58 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -11,7 +11,7 @@ int main() {
     int count=1,candidate=0;
     for(int i=0;i<n;i++){
         int count1=1;
-        for (int j=1;j<n;j++){
+        for (int j=i+1;j<n;j++){
             if(arr[i]==arr[j]){
                 count1+=1;
                 arr[j]=100;
