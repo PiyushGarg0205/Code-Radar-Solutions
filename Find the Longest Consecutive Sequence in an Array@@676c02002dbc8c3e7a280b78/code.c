ce618c7cf9b946e15cec5f731a34059fa63b9260
Piyush Garg <piyushgarg0205@gmail.com>
2025-03-19 19:37:56 +0530

    Solution to Find the Longest Consecutive Sequence in an Array

--- Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
+++ Find the Longest Consecutive Sequence in an Array@@676c02002dbc8c3e7a280b78/code.c
@@ -11,7 +11,7 @@ int main(){
     for(int i=0;i<n;i++){
         if(arr[i]>arr[i+1]){
             int a=arr[i];
-            arr[i]=arr[j];
+            arr[i]=arr[i+1];
             arr[j]=a;
         }
     }
