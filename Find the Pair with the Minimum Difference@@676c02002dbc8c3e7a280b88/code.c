354c9f31919dabb0fddf3cb2642aaa769c034e20
Piyush Garg <piyushgarg0205@gmail.com>
2025-03-20 21:01:41 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,42 +1,41 @@
-#include <stdio.h>
-
-int main() {
+#include<stdio.h>
+int main(){
     int n;
-    scanf("%d", &n);
-
-    if (n == 1) {
-        printf("-1");
-        return 0;
-    }
-
+    scanf("%d",&n);
     int arr[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
-
-    int min = 10000;
+    int min=10000;
     int minimum[2];
-
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) { // Avoid duplicate and self-check
-            int sub = arr[j] - arr[i];
-            if (sub < 0) sub = -sub;
-
-            if (sub < min) {
-                min = sub;
-                minimum[0] = arr[i];
-                minimum[1] = arr[j];
-            }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+        if(i!=j){
+        int sub=arr[j]-arr[i];
+        if(sub<0){
+            sub=sub*-1;
         }
+        if(sub<min){
+            min=sub;
+            minimum[0]=arr[j];
+            minimum[1]=arr[i];
+        }
+        else if(sub==min){
+            minimum[0]=arr[j]<minimum[0]?arr[j]:minimum[0];
+            minimum[1]=arr[i]<minimum[1]?arr[j]:minimum[1];
+        }
+    }}
     }
-
-    // Print in sorted order
-    if (minimum[0] > minimum[1]) {
-        int temp = minimum[0];
-        minimum[0] = minimum[1];
-        minimum[1] = temp;
+    if(minimum[0]>minimum[1]){
+        int c=minimum[0];
+        minimum[0]=minimum[1];
+        minimum[1]=c;
     }
-
-    printf("%d %d", minimum[0], minimum[1]);
-    return 0;
+    if(n==1){
+        printf("-1");
+    }
+    else{
+    for(int i=0;i<2;i++){
+        printf("%d ",minimum[i]);
+    }}
 }
