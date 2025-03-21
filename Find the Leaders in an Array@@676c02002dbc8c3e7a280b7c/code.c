c538486fc68b99029ad654e47d0528aba4239ade
Piyush Garg <piyushgarg0205@gmail.com>
2025-03-21 12:27:04 +0530

    Solution to Find the Leaders in an Array

--- Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
+++ Find the Leaders in an Array@@676c02002dbc8c3e7a280b7c/code.c
@@ -1,43 +1,49 @@
 #include<stdio.h>
-int main(){
+int main() {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    if(n==1){
-        printf("%d",arr[0]);
+    if (n == 1) {
+        printf("%d", arr[0]);
+        return 0;
     }
+
     int maxarr[n];
-    for(int i=0;i<n;i++){
-        maxarr[i]=10000;
+    for (int i = 0; i < n; i++) {
+        maxarr[i] = 10000;
     }
-    int count=0;
-    for(int i=0;i<n;i++){
-        int max=0;
-        int x=1;
-        if(i!=n-1){
-        for(int j=i;j<n;j++){
-        if(max<arr[j]){
-            max=arr[j];
-            for(int i=0;i<n;i++){
-                if(maxarr[i]==max){
-                    x=0;
-                    break;
+
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        int max = 0;
+        int x = 1;
+
+        if (i != n - 1) {
+            for (int j = i; j < n; j++) {
+                if (max < arr[j]) {
+                    max = arr[j];
+                    for (int k = 0; k < count; k++) {
+                        if (maxarr[k] == max) {
+                            x = 0;
+                            break;
+                        }
+                    }
+
+                    if (x == 1) {
+                        maxarr[count++] = max;
+                    }
                 }
             }
-            if(x==1){
-                maxarr[count++]=max;
-            }
         }
     }
-    }
-    
-    }
-    for(int i=0;i<n;i++){
-        if(maxarr[i]!=10000){
-            printf("%d ",maxarr[i]);
+    for (int i = 0; i < count; i++) { 
+        if (maxarr[i] != 10000) {
+            printf("%d ", maxarr[i]);
         }
     }
+
+    return 0;
 }
