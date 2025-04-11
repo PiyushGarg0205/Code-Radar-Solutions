518440329bf8742e8cec909b15a35cbcfc3f9ca5
Piyush Garg <piyushgarg0205@gmail.com>
2025-04-11 11:17:36 +0530

    Solution to Find the Most Frequent Character

--- Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
+++ Find the Most Frequent Character@@676c039d80e1714600f67647/code.c
@@ -1,31 +1,44 @@
-#include<stdio.h>
-#include<string.h>
-int main(){
+#include <stdio.h>
+#include <string.h>
+
+int main() {
     char c[50];
-    fgets(c,sizeof(c),stdin);
-    int len=strlen(c);
-    int maxcount=1;
-    char x;
-    for(int i=0;i<len;i++){
-        int count=1;
-        for(int j=0;j<len;j++){
-            if((i!=j)&&(c[i]==c[j])){
-                count+=1;
+    fgets(c, sizeof(c), stdin);
+
+    // Remove newline character if present
+    size_t len = strlen(c);
+    if (c[len - 1] == '\n') {
+        c[len - 1] = '\0';
+        len--;
+    }
+
+    int maxcount = 0;
+    char x = '~'; // '~' is high in ASCII; ensures lexicographical comparison
+
+    for (int i = 0; i < len; i++) {
+        int count = 1;
+        for (int j = 0; j < len; j++) {
+            if (i != j && c[i] == c[j]) {
+                count++;
             }
         }
-        if(maxcount<count){
-            if(c[i]<x){
-            x=c[i];}
+
+        if (count > maxcount) {
+            maxcount = count;
+            x = c[i];
+        } else if (count == maxcount) {
+            if (c[i] < x) {
+                x = c[i];
+            }
         }
-        else if(maxcount==count){
-            if(c[i]<x){
-            x=c[i];}
-        }
     }
-    if(maxcount==1){
-        printf("%c",c[len-1]);
+
+    // If all characters are unique
+    if (maxcount == 1) {
+        printf("%c", c[len - 1]);
+    } else {
+        printf("%c", x);
     }
-    else{
-        printf("%c",x);
-    }
+
+    return 0;
 }
