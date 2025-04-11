8c6ad91f476574a4d72eb9358492afbcaf6d8806
Piyush Garg <piyushgarg0205@gmail.com>
2025-04-11 11:28:02 +0530

    Solution to Replace a Character in a String

--- Replace a Character in a String@@676c039d80e1714600f67648/code.c
+++ Replace a Character in a String@@676c039d80e1714600f67648/code.c
@@ -3,15 +3,15 @@
 int main(){
     char c[50];
     fgets(c,sizeof(c),stdin);
-    if (c[len - 1] == '\n') {
-        c[len - 1] = '\0';
-        len--;
-    }
     char original;
     scanf("%c",&original);
     char newchar;
     scanf("%c",&newchar);
     int length=strlen(c);
+    if (c[length - 1] == '\n') {
+        c[length - 1] = '\0';
+        length--;
+    }
     for(int i=0;i<length;i++){
         if(c[i]==original){
             c[i]=newchar;
